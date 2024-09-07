0aa8e090a3946b943e3e4b33803f5ffa6d4feaf8
João Victor <larger13@hotmail.com>
2024-09-07 19:18:12 -0300

    Salvando tasks no localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,84 @@ import './global.css';
 import styles from './App.module.css';
 import { Header } from './components/Header';
 import { PlusCircle } from '@phosphor-icons/react';
+import {
+    ChangeEvent,
+    FormEvent,
+    InvalidEvent,
+    useEffect,
+    useState,
+} from 'react';
+
+interface Task {
+    id: string;
+    content: string;
+    completed: boolean;
+}
 
 function App() {
+    const [newTaskText, setNewTaskText] = useState('');
+    const [tasks, setTasks] = useState<Task[]>(() => getTasksLocalStorage());
+
+    useEffect(() => {
+        saveToLocalStorage();
+    }, [tasks]);
+
+    function handleCreateNewTask(event: FormEvent) {
+        event.preventDefault();
+        const newTaskList = [
+            ...tasks,
+            {
+                id: new Date().getTime().toString(),
+                content: newTaskText,
+                completed: false,
+            },
+        ];
+        setTasks(newTaskList);
+        setNewTaskText('');
+    }
+
+    function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+        event.target.setCustomValidity('');
+        setNewTaskText(event.target.value);
+    }
+
+    function getTasksLocalStorage(): Task[] {
+        const storedValue = localStorage.getItem('todolist-ignite');
+        return storedValue ? JSON.parse(storedValue) : [];
+    }
+
+    function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>) {
+        event.target.setCustomValidity('Esse campo é obrigatório!');
+    }
+
+    function saveToLocalStorage() {
+        localStorage.setItem('todolist-ignite', JSON.stringify(tasks));
+    }
+
     return (
         <>
             <Header />
             <div className={styles.wrapper}>
-                <form className={styles.newTaskForm}>
-                    <input placeholder="Adicione uma nova tarefa" />
+                <form
+                    className={styles.newTaskForm}
+                    onSubmit={handleCreateNewTask}
+                >
+                    <input
+                        type="text"
+                        placeholder="Adicione uma nova tarefa"
+                        onChange={handleNewTaskChange}
+                        onInvalid={handleNewTaskInvalid}
+                        required
+                        value={newTaskText}
+                    />
                     <button type="submit">
                         Criar
                         <PlusCircle size={20} />
                     </button>
                 </form>
+                {tasks.map((task) => {
+                    return <p key={task.id}>{task.content}</p>;
+                })}
             </div>
         </>
     );
