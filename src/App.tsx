bf0d9f9ffb4b5ce9423211fc2bc18a9cbe51e4bd
João Victor <larger13@hotmail.com>
2024-09-07 18:18:16 -0300

    Criando o form de nova tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,23 @@ import './global.css';
 
 import styles from './App.module.css';
 import { Header } from './components/Header';
+import { PlusCircle } from '@phosphor-icons/react';
 
 function App() {
-    return <Header />;
+    return (
+        <>
+            <Header />
+            <div className={styles.wrapper}>
+                <form className={styles.newTaskForm}>
+                    <input placeholder="Adicione uma nova tarefa" />
+                    <button type="submit">
+                        Criar
+                        <PlusCircle size={20} />
+                    </button>
+                </form>
+            </div>
+        </>
+    );
 }
 
 export default App;
